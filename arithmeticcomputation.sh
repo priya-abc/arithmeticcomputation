62b277ece75189a3594132c93d5dd78e4a336b9b
priya-abc <priyapriya6598@gmail.com>
2020-11-03 18:56:01 +0530

    Added: Inputs initialized

--- arithmeticcomputation.sh
+++ arithmeticcomputation.sh
@@ -6,4 +6,6 @@ read -p "Enter 1st integer : " a
 read -p "Enter 2nd integer : " b
 read -p "Enter 3rd integer : " c
 ari1=$(($a+$b*$c))
+ari2=$(($a*$b+$c))
 echo "arithmetic calc" $ari1
+echo "arithmetic calc" $ari2
