9cdd5bcf52ea527a388e228de665461be5d18b27
priya-abc <priyapriya6598@gmail.com>
2020-11-03 18:30:46 +0530

    Added: Inputs initialized

--- arithmeticcomputation.sh
+++ arithmeticcomputation.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 echo "welcome to arithmetic computation"
+echo
+#Read inputs from user
+read -p "Enter 1st integer : " a
+read -p "Enter 2nd integer : " b
+read -p "Enter 3rd integer : " c
 
